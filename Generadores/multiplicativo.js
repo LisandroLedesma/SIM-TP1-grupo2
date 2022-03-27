f19dbed16da52e26cc157e4f2452e3b31c01594e
LisandroLedesma <lichaledesma31@gmail.com>
2022-03-26 22:42:07 -0300

    Refactor. Multiplicativo listo, pero sus.

---
+++ Generadores/multiplicativo.js
@@ -0,0 +1,120 @@
+//metodo multiplicativo
+
+const btnTestM = document.getElementById('btnTestMultiplicativo');
+
+// Para el test
+var nroRnd;
+
+function generar_semillas(a, semilla, m) {
+    let prox_semilla = (a * semilla) % m;
+    return prox_semilla
+}
+
+function generar_aleatorio_multiplicativo(a, semilla, m, cant_random) {
+    let random = [];
+    for (let i = 0; i < cant_random; i++) {
+        semilla = generar_semillas(a, semilla, m);
+        let rnd = semilla / m;
+        let rnd_redondeado = rnd.toFixed(4);
+
+        const valor_random = new ValorRandom(i + 1, semilla, rnd_redondeado);
+        prox_semilla = semilla;
+
+        let row = {
+            "n": valor_random.n,
+            "Semilla": valor_random.semilla,
+            "Numero": valor_random.nro_random,
+        }
+
+        random.push(row);
+
+    }
+
+    return random
+}
+
+const button = document.getElementById('btnSimMultiplicativo');
+
+
+const borrarTablaM = () => {
+
+    const eGridDiv = document.querySelector('#myGridMult');
+    const testGridDivM = document.querySelector('#gridTestMultiplicativo');
+
+
+    button.disabled = false;
+    btnTestM.disabled = false;
+
+
+    let child = eGridDiv.lastElementChild;
+    while (child) {
+        eGridDiv.removeChild(child);
+        child = eGridDiv.lastElementChild;
+    }
+
+    let childT = testGridDivM.lastElementChild;
+    while (childT) {
+        testGridDivM.removeChild(childT);
+        childT = testGridDivM.lastElementChild;
+    }
+}
+
+const simularMultiplicativo = () => {
+
+    const eGridDiv = document.querySelector('#myGridMult');
+
+    button.disabled = true;
+
+
+    // Definir columnas
+    let columnDefs = [
+        { field: "n" },
+        { field: "Semilla" },
+        { field: "Numero" },
+    ];
+
+
+    // Validar inputs
+    let a = document.getElementById('mm-a').value;
+    let m = document.getElementById('mm-m').value;
+    let n = document.getElementById('mm-n').value;
+    let semilla = document.getElementById('mm-semilla').value;
+
+    //validacion para campos vacios
+    if (a === '' || m === '' || n === '' || semilla === '') {
+        alert('Por favor, llene todos los campos');
+        button.disabled = false;
+    }
+
+    //validacion para campos negativos
+    if (a < 0 || m < 0 || n < 0 || semilla < 0) {
+        alert('Los valores deben ser positivos');
+        button.disabled = false;
+    }
+
+    //validacion para semilla impar y primo
+    //if (semilla % 2 == 0){
+    //    alert('La semilla debe ser un numero impar y primo')
+    //}
+
+    // Definir filas
+    let rowData = generar_aleatorio_multiplicativo(a, semilla, m, n);
+    nroRnd = rowData;
+
+    // Matchea colunas y filas
+    let gridOptions = {
+        columnDefs: columnDefs,
+        rowData: rowData
+    };
+
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+}
+
+class ValorRandom {
+    constructor(n, semilla, nro_random) {
+        this.n = n
+        this.semilla = semilla
+        this.nro_random = nro_random
+    }
+}
---
+++ test_chi_multiplicativo.js
@@ -0,0 +1,161 @@
+const buttonTestM = document.getElementById('btnTestMultiplicativo');
+
+const chiM = [
+    [4, 9.49],
+    [9, 16.92],
+    [14, 23.68],
+    [19, 30.14]
+];
+
+const getNumerosM = () => {
+
+    filas = window.nroRnd;
+    let aux = [];
+
+    filas.forEach(fila => {
+        aux.push(fila.Numero);
+    });
+
+
+    return aux;
+}
+
+const arrayMinM = (arr) => {
+    return arr.reduce(function(p, v) {
+        return (p < v ? p : v);
+    });
+}
+
+const arrayMaxM = (arr) => {
+    return arr.reduce(function(p, v) {
+        return (p > v ? p : v);
+    });
+}
+
+const testM = () => {
+
+
+    const select = document.getElementById("intMultiplicativo");
+    const intervalos = select.value;
+
+    const numeros = getNumerosM();
+    const max = arrayMaxM(numeros);
+    const min = arrayMinM(numeros);
+    const paso = (max - min) / intervalos;
+
+
+    let [suma, filas] = sumatoriaM(numeros, min, max, intervalos, paso);
+
+    let res = pruebaM(intervalos, suma);
+
+    console.log("test: ", res);
+
+    generarTablaM(filas);
+}
+
+const sumatoriaM = (nros, minimo, maximo, int, paso) => {
+    let filas = [];
+    let suma = 0;
+    let min = minimo;
+
+    for (let i = 0; i < int; i++) {
+
+        if (i == 0) {
+            lim_inf = Number(min);
+            lim_sup = (Number(min) + Number(paso)).toFixed(4);
+        } else {
+            lim_inf = Number(lim_sup);
+            lim_sup = (Number(lim_sup) + Number(paso)).toFixed(4);
+        }
+
+        let fila = new Object();
+
+        let mc = (lim_inf + (lim_sup - lim_inf) / 2).toFixed(4);
+        fila.marca_clase = mc;
+        fila.lim_inf = lim_inf;
+        fila.lim_sup = Number(lim_sup);
+        fila.fo = frecObsM(nros, lim_inf, lim_sup);
+        fila.fe = nros.length / int;
+        fila.estadistico = ((fila.fe - fila.fo) ** 2) / fila.fe;
+
+        suma = (Number(suma) + Number(fila.estadistico)).toFixed(4);
+
+        filas.push(fila);
+
+    }
+
+    return [suma, filas];
+
+}
+
+const frecObsM = (nros, inf, sup) => {
+    fo = 0
+
+    ord = nros.sort();
+
+    for (let i = 0; i < ord.length; i++) {
+        if ((i + 1) !== ord.length) {
+            if (ord[i] >= inf && ord[i] < sup) {
+                fo += 1
+            }
+        } else {
+            if (ord[i] >= inf && ord[i] <= sup) {
+                fo += 1
+            }
+        }
+    }
+
+    return fo
+}
+
+const pruebaM = (int, suma) => {
+    let v = int - 1;
+    let res = false;
+    let valor_tabla = 0;
+
+    chiM.forEach((par) => {
+        if (par[0] === v) {
+            valor_tabla = par[1];
+        }
+    });
+
+    if (suma <= valor_tabla) {
+        res = true;
+    }
+
+    return res;
+}
+
+const generarTablaM = (filas) => {
+    const eGridDiv = document.querySelector('#gridTestMultiplicativo');
+
+    let columnDefs = [
+        { field: "LimInf" },
+        { field: "LimSup" },
+        { field: "MC" },
+        { field: "Fe" },
+        { field: "Fo" },
+        { field: "Estadístico" },
+    ];
+
+    let rowData = [];
+    filas.forEach((fila) => {
+        let row = {
+            "LimInf": fila.lim_inf,
+            "LimSup": fila.lim_sup,
+            "MC": fila.marca_clase,
+            "Fe": fila.fe,
+            "Fo": fila.fo,
+            "Estadístico": fila.estadistico,
+        }
+        rowData.push(row);
+    })
+
+    let gridOptions = {
+        columnDefs: columnDefs,
+        rowData: rowData
+    };
+
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+}
